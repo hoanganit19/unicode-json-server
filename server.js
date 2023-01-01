8f013e909a8c6d1e6b13c9a5b7ad55cb44b9dbec
Hoang An <contact@unicode.vn>
2023-01-01 22:56:37 +0700

    Update db.json

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const fs = require("fs");
-const os = require("os");
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-//const router = jsonServer.router("db.json");
-fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
-  if (err) console.log(err);
-  else console.log("copy file succeed to" + os.tmpdir());
-});
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -19,6 +12,10 @@ server.use(
     "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
+
+router.render = (req, res) => {
+  console.log(res);
+};
 
 server.use(router);
 server.listen(3000, () => {
